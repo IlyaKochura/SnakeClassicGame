a082f0314da17083489a7c7bde3490e333b1a195
IlyaKochura <ikochura@inbox.ru>
2022-07-06 10:24:56 +0300

    Revert "драфт"
    This reverts commit 467a66ec5c4195b3f56436bc57a9e5eca998fe5a.

--- Assets/MainScripts/Controller.cs
+++ Assets/MainScripts/Controller.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 using Random = System.Random;
 
@@ -11,10 +10,7 @@ namespace MainScripts
     public class Controller : MonoBehaviour
     {
         [SerializeField] private List<ButtonScript> buttonScripts;
-<<<<<<< Updated upstream
         [SerializeField] private List<GameObject> slaveElement;
-=======
->>>>>>> Stashed changes
         [SerializeField] private Transform leader;
         [SerializeField] private float speed;
         [SerializeField] private GameObject endGameTitle;
@@ -23,13 +19,7 @@ namespace MainScripts
         [SerializeField] private Transform leaderBodySpawn;
         [SerializeField] private Text score;
         [SerializeField] private SpawnBodyButtonScript spawnButton;
-        private List<GameObject> _slaveElement = new();
-        private List<Vector3> _nextPosition = new();
         private int _id;
-<<<<<<< Updated upstream
-=======
-        private int _currentId;
->>>>>>> Stashed changes
         private MoveState _movementState = MoveState.MoveUp;
         private Axis _axis = Axis.AxisY;
 
@@ -55,67 +45,34 @@ namespace MainScripts
                 buttonScripts[i].click = () => Transmitter(id);
             }
 
+            buttonScripts[5].click = () => AddBodyAndEat(leaderBodySpawn.localPosition);
             buttonScripts[4].click = () => Restart();
-            buttonScripts[5].click = () => AddBodyAndEat();
         }
 
         void Update()
         {
             switch (_axis)
             {
-                case Axis.AxisY:
+                case Axis.AxisY :
                     if (leader.localPosition.y % 100 > -1 && leader.localPosition.y % 100 < 1)
                     {
                         Direction(_id);
-                        UpdateCashPosition();
                     }
-
                     break;
-
-                case Axis.AxisX:
+                
+                case Axis.AxisX :
                     if (leader.localPosition.x % 100 > -1 && leader.localPosition.x % 100 < 1)
                     {
                         Direction(_id);
-                        UpdateCashPosition();
                     }
-
                     break;
             }
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
             Move();
-        }
-
-        private void UpdateCashPosition()
-        {
-            _nextPosition.Clear();
-
-            if (_slaveElement.Count <= 0) return;
-            for (var i = 0; i < _slaveElement.Count; i++)
-            {
-                if (i <= 0)
-                {
-                    _nextPosition.Add(leader.localPosition);
-                }
-                else
-                {
-                    _nextPosition.Add(_slaveElement[i - 1].transform.localPosition);
-                }
-
-                //_slaveElement[i].transform.localPosition = _nextPosition[i];
-            }
-            
-            for (var i = 0; i < _slaveElement.Count; i++)
-            {
-                _slaveElement[i].transform.localPosition = _nextPosition[i];
-            }
         }
 
         void Direction(int dir)
         {
-            var i = (MoveState)dir;
+            var i = (MoveState) dir;
 
             switch (i)
             {
@@ -124,36 +81,30 @@ namespace MainScripts
                     {
                         return;
                     }
-
                     break;
                 case MoveState.MoveDown:
                     if (_movementState == MoveState.MoveUp)
                     {
                         return;
                     }
-
                     break;
                 case MoveState.MoveLeft:
                     if (_movementState == MoveState.MoveRight)
                     {
                         return;
                     }
-
                     break;
                 case MoveState.MoveRight:
                     if (_movementState == MoveState.MoveLeft)
                     {
                         return;
                     }
-
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
             }
-
             _movementState = i;
         }
-<<<<<<< Updated upstream
         
         private void AddBodyAndEat(Vector3 position)
         {
@@ -162,30 +113,10 @@ namespace MainScripts
                 bodyElement.transform.localPosition = position;
 
                 MoveEatAfterAdd();
-=======
 
-
-        private void AddBodyAndEat()
-        {
-            // for (int i = 0; i < slaveElement.Count; i++)
-            // {
-            //     if (leader.localPosition == slaveElement[i].transform.localPosition)
-            //     {
-            //         endGameTitle.SetActive(true);
-            //         Time.timeScale = 0;
-            //     }   
-            // }
-            
-                var bodyElement = Instantiate(bodyPrefab, leaderBodySpawn);
-                _slaveElement.Add(bodyElement);
-                bodyElement.transform.localPosition = leader.localPosition;
-                
-            
->>>>>>> Stashed changes
-
-            score.text = Convert.ToString(_slaveElement.Count);
+                score.text = Convert.ToString(slaveElement.Count);
         }
-
+        
         private void GetDirection(Transform lTransform)
         {
             int posX;
@@ -200,20 +131,19 @@ namespace MainScripts
                         posX = Convert.ToInt32(Math.Round(leader.localPosition.x / 100) * 100);
                         leader.localPosition = new Vector3(posX, leader.localPosition.y);
                     }
-
                     break;
-
+                
                 case MoveState.MoveDown:
                     lTransform.Translate(Vector3.down * speed * Time.deltaTime);
                     if (leader.localPosition.x % 100 != 0)
                     {
                         _axis = Axis.AxisY;
-                        posX = Convert.ToInt32(Math.Round(leader.localPosition.x / 100) * 100);
-                        leader.localPosition = new Vector3(posX, leader.localPosition.y);
+                       posX = Convert.ToInt32(Math.Round(leader.localPosition.x / 100) * 100);
+                       leader.localPosition = new Vector3(posX, leader.localPosition.y);
                     }
-
+                    
                     break;
-
+                
                 case MoveState.MoveLeft:
                     lTransform.Translate(Vector3.left * speed * Time.deltaTime);
                     if (leader.localPosition.y % 100 != 0)
@@ -222,9 +152,9 @@ namespace MainScripts
                         posY = Convert.ToInt32(Math.Round(leader.localPosition.y / 100) * 100);
                         leader.localPosition = new Vector3(leader.localPosition.x, posY);
                     }
-
+                    
                     break;
-
+                
                 case MoveState.MoveRight:
                     lTransform.Translate(Vector3.right * speed * Time.deltaTime);
                     if (leader.localPosition.y % 100 != 0)
@@ -233,7 +163,6 @@ namespace MainScripts
                         posY = Convert.ToInt32(Math.Round(leader.localPosition.y / 100) * 100);
                         leader.localPosition = new Vector3(leader.localPosition.x, posY);
                     }
-
                     break;
             }
         }
@@ -246,30 +175,18 @@ namespace MainScripts
 
             eat.transform.localPosition = new Vector3(posX * 100, posY * 100, 0);
         }
-
+        
         private void Move()
         {
             GetDirection(leader);
             var pos = leader.localPosition;
-<<<<<<< Updated upstream
             foreach (var element in slaveElement)
             {
                 var newLoc = element.transform.localPosition;
                 element.transform.localPosition = pos;
                 pos = newLoc;
             }
-=======
-            // foreach (var element in _slaveElement)
-            // {
-            //     var newLoc = element.transform.localPosition;
-            //     element.transform.localPosition = new Vector3(newLoc.x + 100, newLoc.y + 100);
-            //     element.transform.localPosition = pos;
-            //     pos = newLoc;
-            // }
-
->>>>>>> Stashed changes
             MovingEdgesScene();
-            //AddBodyAndEat();
         }
         
         private void Restart()
@@ -294,7 +211,7 @@ namespace MainScripts
             {
                 leader.localPosition = new Vector3(1300, leader.localPosition.y, 0);
             }
-
+            
             if (leader.localPosition.x > 1300)
             {
                 leader.localPosition = new Vector3(-800, leader.localPosition.y, 0);
@@ -305,45 +222,5 @@ namespace MainScripts
         {
             _id = id;
         }
-<<<<<<< Updated upstream
-=======
-
-
-        // void DirectionTemporarySwitch()
-        // {
-        //     var mov = _movementState;
-        //     
-        //     switch (_movementState)
-        //     {
-        //         case MoveState.MoveUp:
-        //             if (_movementState == MoveState.MoveDown)
-        //             {
-        //                 return;
-        //             }
-        //             break;
-        //         case MoveState.MoveDown:
-        //             if (_movementState == MoveState.MoveUp)
-        //             {
-        //                 return;
-        //             }
-        //             break;
-        //         case MoveState.MoveLeft:
-        //             if (_movementState == MoveState.MoveRight)
-        //             {
-        //                 return;
-        //             }
-        //             break;
-        //         case MoveState.MoveRight:
-        //             if (_movementState == MoveState.MoveLeft)
-        //             {
-        //                 return;
-        //             }
-        //             break;
-        //         default:
-        //             throw new ArgumentOutOfRangeException();
-        //     }
-        //     _movementState = mov;
-        // }
->>>>>>> Stashed changes
     }
 }
